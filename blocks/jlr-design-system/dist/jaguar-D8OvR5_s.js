749df135debacbe90e16140656a0d9192de08786
Rohit Utekar <rohit.utekar@mirumagency.com>
2026-09-24 15:55:34 +0530

    Add JLR Design System block with CSS and JS integration
    - Created jlr-design-system.css to define block-level styles and import EDS styles.
    - Implemented jlr-design-system.js to mount the JLR UI component tree into a scoped container.

---
+++ blocks/jlr-design-system/dist/jaguar-D8OvR5_s.js
@@ -0,0 +1,255 @@
+import { i as e, n as t, t as n } from "./jsx-runtime-C2kUopEs.js";
+//#region ../../packages/icons/src/custom/brands/jaguar/ChevronIcon.tsx
+var r = /* @__PURE__ */ e(t(), 1), i = n(), a = (0, r.forwardRef)(function({ className: e, width: t = 14, height: n = 14, ...r }, a) {
+	return /* @__PURE__ */ (0, i.jsx)("svg", {
+		ref: a,
+		xmlns: "http://www.w3.org/2000/svg",
+		viewBox: "0 0 14 14",
+		width: t,
+		height: n,
+		fill: "none",
+		className: e,
+		...r,
+		children: /* @__PURE__ */ (0, i.jsx)("path", {
+			d: "M12 4.5L7 9.5L2 4.5",
+			stroke: "currentColor",
+			strokeWidth: "0.5",
+			strokeLinecap: "square"
+		})
+	});
+});
+a.displayName = "ChevronIcon";
+//#endregion
+//#region ../../packages/icons/src/custom/brands/jaguar/CloseIcon.tsx
+var o = (0, r.forwardRef)(function({ className: e, width: t = 21, height: n = 21, ...r }, a) {
+	return /* @__PURE__ */ (0, i.jsx)("svg", {
+		ref: a,
+		xmlns: "http://www.w3.org/2000/svg",
+		viewBox: "0 0 21 21",
+		width: t,
+		height: n,
+		fill: "none",
+		className: e,
+		...r,
+		children: /* @__PURE__ */ (0, i.jsx)("path", {
+			d: "M10.5 11.0903L17.0347 17.625L17.625 17.0347L11.0903 10.5L17.625 3.96527L17.0347 3.375L10.5 9.90973L3.96527 3.375L3.375 3.96527L9.90973 10.5L3.375 17.0347L3.96527 17.625L10.5 11.0903Z",
+			fill: "currentColor"
+		})
+	});
+});
+o.displayName = "CloseIcon";
+//#endregion
+//#region ../../packages/icons/src/custom/brands/jaguar/GlobeIcon.tsx
+var s = (0, r.forwardRef)(function({ className: e, width: t = 18, height: n = 18, ...r }, a) {
+	return /* @__PURE__ */ (0, i.jsxs)("svg", {
+		ref: a,
+		xmlns: "http://www.w3.org/2000/svg",
+		viewBox: "0 0 18 18",
+		width: t,
+		height: n,
+		fill: "none",
+		className: e,
+		...r,
+		children: [
+			/* @__PURE__ */ (0, i.jsx)("path", {
+				stroke: "currentColor",
+				strokeWidth: "0.616071",
+				d: "M8.6259 16.2557C12.8397 16.2557 16.2557 12.8397 16.2557 8.6259C16.2557 4.41208 12.8397 0.996094 8.6259 0.996094C4.41208 0.996094 0.996094 4.41208 0.996094 8.6259C0.996094 12.8397 4.41208 16.2557 8.6259 16.2557Z"
+			}),
+			/* @__PURE__ */ (0, i.jsx)("path", {
+				stroke: "currentColor",
+				strokeWidth: "0.616071",
+				strokeLinecap: "round",
+				d: "M8.62455 16.2557C7.15735 16.2557 5.9707 12.8405 5.9707 8.6259C5.9707 4.41134 7.15735 0.996094 8.62455 0.996094"
+			}),
+			/* @__PURE__ */ (0, i.jsx)("path", {
+				stroke: "currentColor",
+				strokeWidth: "0.616071",
+				strokeLinecap: "round",
+				d: "M8.625 16.2557C10.0922 16.2557 11.2788 12.8405 11.2788 8.6259C11.2788 4.41134 10.0922 0.996094 8.625 0.996094"
+			}),
+			/* @__PURE__ */ (0, i.jsx)("path", {
+				stroke: "currentColor",
+				strokeWidth: "0.616071",
+				strokeLinecap: "round",
+				d: "M1.6582 5.9707L15.5909 5.9707"
+			}),
+			/* @__PURE__ */ (0, i.jsx)("path", {
+				stroke: "currentColor",
+				strokeWidth: "0.616071",
+				strokeLinecap: "round",
+				d: "M1.6582 11.2793L15.5909 11.2793"
+			})
+		]
+	});
+});
+s.displayName = "GlobeIcon";
+//#endregion
+//#region ../../packages/icons/src/custom/brands/jaguar/InfoIcon.tsx
+var c = (0, r.forwardRef)(function({ className: e, width: t = 14, height: n = 14, ...r }, a) {
+	return /* @__PURE__ */ (0, i.jsxs)("svg", {
+		ref: a,
+		xmlns: "http://www.w3.org/2000/svg",
+		viewBox: "0 0 14 14",
+		width: t,
+		height: n,
+		fill: "none",
+		className: e,
+		...r,
+		children: [/* @__PURE__ */ (0, i.jsx)("circle", {
+			cx: "7.00008",
+			cy: "7.00008",
+			r: "6",
+			strokeWidth: "0.666667"
+		}), /* @__PURE__ */ (0, i.jsx)("path", {
+			d: "M6.72372 6.088H7.28372V10H6.72372V6.088ZM7.00372 5.232C6.89172 5.232 6.79572 5.19467 6.71572 5.12C6.64105 5.04 6.60372 4.94667 6.60372 4.84C6.60372 4.728 6.64105 4.63467 6.71572 4.56C6.79572 4.48 6.89172 4.44 7.00372 4.44C7.11039 4.44 7.20105 4.48 7.27572 4.56C7.35572 4.63467 7.39572 4.728 7.39572 4.84C7.39572 4.94667 7.35572 5.04 7.27572 5.12C7.20105 5.19467 7.11039 5.232 7.00372 5.232Z",
+			fill: "currentColor",
+			stroke: "none"
+		})]
+	});
+});
+c.displayName = "InfoIcon";
+//#endregion
+//#region ../../packages/icons/src/custom/brands/jaguar/ErrorIcon.tsx
+var l = (0, r.forwardRef)(function({ className: e, width: t = 14, height: n = 14, ...r }, a) {
+	return /* @__PURE__ */ (0, i.jsxs)("svg", {
+		ref: a,
+		xmlns: "http://www.w3.org/2000/svg",
+		viewBox: "0 0 14 14",
+		width: t,
+		height: n,
+		fill: "none",
+		className: e,
+		...r,
+		children: [/* @__PURE__ */ (0, i.jsx)("path", {
+			d: "M6.31641 2.41797C6.61684 1.88669 7.38316 1.88669 7.68359 2.41797L12.3184 10.6182C12.614 11.1413 12.2357 11.7891 11.6348 11.7891H2.36523C1.76431 11.7891 1.38595 11.1413 1.68164 10.6182L6.31641 2.41797Z",
+			fill: "currentColor",
+			stroke: "currentColor",
+			strokeWidth: "1.19048"
+		}), /* @__PURE__ */ (0, i.jsx)("path", {
+			d: "M7.03571 10C6.88839 10 6.76228 9.9456 6.65737 9.83681C6.55246 9.72801 6.5 9.59722 6.5 9.44444C6.5 9.29167 6.55246 9.16088 6.65737 9.05208C6.76228 8.94329 6.88839 8.88889 7.03571 8.88889C7.18304 8.88889 7.30915 8.94329 7.41406 9.05208C7.51897 9.16088 7.57143 9.29167 7.57143 9.44444C7.57143 9.59722 7.51897 9.72801 7.41406 9.83681C7.30915 9.9456 7.18304 10 7.03571 10ZM6.5 8.33333V5H7.57143V8.33333H6.5Z",
+			fill: "white"
+		})]
+	});
+});
+l.displayName = "ErrorIcon";
+//#endregion
+//#region ../../packages/icons/src/custom/brands/jaguar/SelectArrowIcon.tsx
+var u = (0, r.forwardRef)(function({ className: e, width: t = 28, height: n = 28, ...r }, a) {
+	return /* @__PURE__ */ (0, i.jsx)("svg", {
+		ref: a,
+		xmlns: "http://www.w3.org/2000/svg",
+		viewBox: "0 0 28 28",
+		width: t,
+		height: n,
+		fill: "none",
+		className: e,
+		...r,
+		children: /* @__PURE__ */ (0, i.jsx)("path", {
+			fillRule: "evenodd",
+			clipRule: "evenodd",
+			d: "M13.833 18.3573L6.00935 9.34814L5.44641 9.83701L13.5515 19.1703L14.1145 19.1703L21.9999 10.0891L21.437 9.6003L13.833 18.3573Z",
+			fill: "currentColor",
+			stroke: "currentColor",
+			strokeWidth: "0.3",
+			strokeLinejoin: "bevel"
+		})
+	});
+});
+u.displayName = "SelectArrowIcon";
+//#endregion
+//#region ../../packages/icons/src/custom/brands/jaguar/MapIcon.tsx
+var d = (0, r.forwardRef)(function({ className: e, width: t = 20, height: n = 20, ...r }, a) {
+	return /* @__PURE__ */ (0, i.jsx)("svg", {
+		ref: a,
+		xmlns: "http://www.w3.org/2000/svg",
+		viewBox: "0 0 20 20",
+		width: t,
+		height: n,
+		fill: "none",
+		className: e,
+		...r,
+		children: /* @__PURE__ */ (0, i.jsx)("path", {
+			fillRule: "evenodd",
+			clipRule: "evenodd",
+			d: "M13 12.505L10.43 14.0492C10.3033 14.1233 10.1567 14.1667 10 14.1667C9.84333 14.1667 9.69667 14.1233 9.57167 14.0483L7 12.5033C5.30333 11.4842 4.16667 9.625 4.16667 7.5C4.16667 4.27833 6.77833 1.66667 10 1.66667C13.2225 1.66667 15.8333 4.27833 15.8333 7.5C15.8333 9.625 14.6975 11.4842 13 12.505ZM10 0C5.8575 0 2.5 3.3575 2.5 7.5C2.5 9.95667 3.68083 12.1367 5.50583 13.505L7.49667 14.9967C8.51083 15.7567 9.16667 16.9675 9.16667 18.3333V19.1667C9.16667 19.6275 9.54 20 10 20C10.46 20 10.8333 19.6275 10.8333 19.1667V18.3333C10.8333 16.9675 11.49 15.7567 12.5033 14.9967L14.4958 13.505C16.32 12.1367 17.5 9.95667 17.5 7.5C17.5 3.3575 14.1425 0 10 0ZM10 5C11.3808 5 12.5 6.11917 12.5 7.5C12.5 8.88083 11.3808 10 10 10C8.61917 10 7.5 8.88083 7.5 7.5C7.5 6.11917 8.61917 5 10 5Z",
+			fill: "currentColor"
+		})
+	});
+});
+d.displayName = "MapIcon";
+//#endregion
+//#region ../../packages/icons/src/custom/brands/jaguar/SaveIcon.tsx
+var f = (0, r.forwardRef)(function({ className: e, width: t = 20, height: n = 20, ...r }, a) {
+	return /* @__PURE__ */ (0, i.jsx)("svg", {
+		ref: a,
+		xmlns: "http://www.w3.org/2000/svg",
+		viewBox: "0 0 20 20",
+		width: t,
+		height: n,
+		fill: "none",
+		className: e,
+		...r,
+		children: /* @__PURE__ */ (0, i.jsx)("path", {
+			d: "M4.18769 20C4.07764 20 3.96706 19.9786 3.86182 19.9354C3.54184 19.8034 3.3335 19.4914 3.3335 19.1453V2.50968C3.33296 1.1261 4.45906 0 5.84264 0H14.1292C15.5427 0 16.6923 1.1496 16.6923 2.5631V19.1367C16.6923 19.4818 16.4845 19.7933 16.1656 19.9257C15.8467 20.0582 15.4797 19.9861 15.235 19.7425L9.98912 14.5276L4.79347 19.7473C4.63001 19.9119 4.41045 19.9989 4.18769 19.9989V20ZM9.98592 12.4651C10.2039 12.4651 10.4218 12.5479 10.5885 12.7135L14.9828 17.0817V2.5631C14.9828 2.09247 14.5998 1.70945 14.1292 1.70945H5.84264C5.40139 1.70945 5.04241 2.06843 5.04241 2.50968V17.0747L9.38013 12.7167C9.54734 12.5489 9.76636 12.4651 9.98592 12.4651Z",
+			fill: "currentColor"
+		})
+	});
+});
+f.displayName = "SaveIcon";
+//#endregion
+//#region ../../packages/icons/src/custom/brands/jaguar/MenuIcon.tsx
+var p = (0, r.forwardRef)(function({ className: e, width: t = 19, height: n = 17, ...r }, a) {
+	return /* @__PURE__ */ (0, i.jsxs)("svg", {
+		ref: a,
+		xmlns: "http://www.w3.org/2000/svg",
+		viewBox: "0 0 19 17",
+		width: t,
+		height: n,
+		fill: "none",
+		className: e,
+		...r,
+		children: [
+			/* @__PURE__ */ (0, i.jsx)("line", {
+				x1: "0.5",
+				y1: "0.5",
+				x2: "18.0185",
+				y2: "0.5",
+				stroke: "#333433",
+				strokeLinecap: "round"
+			}),
+			/* @__PURE__ */ (0, i.jsx)("line", {
+				x1: "0.5",
+				y1: "8.5",
+				x2: "18.0185",
+				y2: "8.5",
+				stroke: "#333433",
+				strokeLinecap: "round"
+			}),
+			/* @__PURE__ */ (0, i.jsx)("line", {
+				x1: "0.5",
+				y1: "16.5",
+				x2: "18.0185",
+				y2: "16.5",
+				stroke: "#333433",
+				strokeLinecap: "round"
+			})
+		]
+	});
+});
+p.displayName = "MenuIcon";
+//#endregion
+//#region ../../packages/icons/src/custom/brands/jaguar/index.ts
+var m = {
+	globe: s,
+	chevron: a,
+	selectArrow: u,
+	close: o,
+	info: c,
+	error: l,
+	map: d,
+	save: f,
+	menu: p
+};
+//#endregion
+export { m as jaguarIconOverrides };
---
+++ blocks/jlr-design-system/jlr-design-system.js
@@ -0,0 +1,17 @@
+/*
+ * JLR Design System Block
+ * Mounts the shared @jlr/ui-react component tree (built from the JLR-GlobalCVL-FE
+ * workspace) into a scoped container, so the design system renders inside an
+ * EDS-authored page with no Next.js runtime involved.
+ *
+ * `./dist/eds-poc.js` is vendored from apps/eds-poc's `pnpm --filter eds-poc build`
+ * output. Once the bundle is published to a CDN, replace the relative import below
+ * with the absolute CDN URL and drop the vendored ./dist folder.
+ */
+export default async function decorate(block) {
+  const { mount } = await import('./dist/eds-poc.js');
+  const container = document.createElement('div');
+  container.className = 'jlr-ds-root';
+  block.replaceChildren(container);
+  mount(container);
+}
